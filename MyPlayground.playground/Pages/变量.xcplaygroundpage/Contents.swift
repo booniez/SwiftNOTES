b99046d5b98522f3387a4422d91545cbd5eb58b4
WWGitMore <1490305371@qq.com>
2017-10-17 16:51:19 +0800

    变量

---
+++ MyPlayground.playground/Pages/变量.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+var varA = "这是一个变量"
+print(varA)
+var name = "Jack"
+var msg = "《The Swift Programming Language》"
+
+print("\(name)的资料书是\(msg)")
+
